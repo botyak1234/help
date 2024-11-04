27300ce01e1c0c848b1d5aa8be444ed695b84df2
BOTYA <95568178+botyak1234@users.noreply.github.com>
2024-11-04 21:51:49 +0400

    Update func.py

--- func.py
+++ func.py
@@ -1,5 +1,10 @@
 def new_list(lst):
-    max_index = lst.index(max(lst))
+    max_index = 0
+    max_elem = lst[0]
+    for i in range(len(lst)):
+        if lst[i] > max_elem:
+            max_elem = lst[i]
+            max_index = i
     for i in range(max_index + 1, len(lst)):
         lst[i] -= 1
 
